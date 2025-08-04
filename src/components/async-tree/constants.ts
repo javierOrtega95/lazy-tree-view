57da46eb85e1d73aef1a0fccb0f1f5393ed4b7ea
javierOrtega95 <fjortega@wizzie.io>
2025-08-05 01:42:16 +0200

    refactor: update type imports and improve constant definitions in AsyncTree

--- src/components/async-tree/constants.ts
+++ src/components/async-tree/constants.ts
@@ -1,4 +1,4 @@
-import { FolderNode } from './types'
+import type { DragClassNames, FolderNode } from './types'
 
 const ROOT_ID = crypto.randomUUID()
 
@@ -8,15 +8,14 @@ export const ROOT_NODE: FolderNode = {
   children: [],
 }
 
-export const defaultDnDclassNames = {
+export const defaultDnDclassNames: DragClassNames = {
   dragOver: 'drag-over',
   dragBefore: 'drag-before',
   dragAfter: 'drag-after',
   dropNotAllowed: 'drop-not-allowed',
 }
 
-export const TREE_NODE_INDENTATION = 12
+export const BASE_NODE_INDENTATION = 14
 
-export const DROP_BEFORE_PERCENT = 0.1
+export const DROP_BEFORE_FOLDER_PERCENT = 0.1
 export const DROP_MID_PERCENT = 0.5
-export const DROP_AFTER_FOLDER_PERCENT = 0.7
