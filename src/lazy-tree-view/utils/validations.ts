85c03c4a6f30edd425955be53c53ef614c6f4f15
javierOrtega95 <fjortega@wizzie.io>
2026-01-26 23:01:06 +0100

    refactor: remove unused code

--- src/lazy-tree-view/utils/validations.ts
+++ src/lazy-tree-view/utils/validations.ts
@@ -1,30 +1,4 @@
-import { DropPosition, type MoveData } from '../../types/dnd'
 import type { BaseNode, FolderNode, NodeParents, TreeNode } from '../../types/tree'
-
-export function isValidMove({
-  source,
-  target,
-  position,
-  prevParent,
-  nextParent,
-  nodeParents,
-}: Omit<MoveData, 'tree'> & {
-  nodeParents: NodeParents
-}): boolean {
-  if (isAlreadyInsideFolder(source, target, position)) return false
-
-  if (isFolderNode(source) && isMovingFolderIntoDescendant(source, target, nodeParents)) {
-    return false
-  }
-
-  const isOrderingSameParent = position !== DropPosition.Inside && prevParent?.id === nextParent?.id
-
-  if (isOrderingSameParent && isSamePosition({ source, target, position, prevParent })) {
-    return false
-  }
-
-  return true
-}
 
 function isDescendant(source: TreeNode, target: TreeNode, nodeParents: NodeParents): boolean {
   let currentParent = nodeParents[target.id]
@@ -41,47 +15,9 @@ function isDescendant(source: TreeNode, target: TreeNode, nodeParents: NodeParen
 export function isMovingFolderIntoDescendant(
   source: FolderNode,
   target: TreeNode,
-  nodeParents: NodeParents
+  nodeParents: NodeParents,
 ): boolean {
   if (isDescendant(source, target, nodeParents)) return true
-
-  return false
-}
-
-function isAlreadyInsideFolder(
-  source: TreeNode,
-  target: TreeNode,
-  position: DropPosition
-): boolean {
-  if (position === DropPosition.Inside) {
-    const { children } = target as FolderNode
-
-    if (children.some((child) => child.id === source.id)) return true
-  }
-
-  return false
-}
-
-function isSamePosition({
-  source,
-  target,
-  position,
-  prevParent,
-}: Omit<MoveData, 'tree' | 'nextParent'>): boolean {
-  if (!prevParent) return false
-
-  const { children = [] } = prevParent
-
-  const sourceIndex = children.findIndex((child) => child.id === source.id)
-  const targetIndex = children.findIndex((child) => child.id === target.id)
-
-  if (sourceIndex === -1 || targetIndex === -1) return false
-
-  const newTargetIndex = targetIndex > sourceIndex ? targetIndex - 1 : targetIndex
-
-  const newPosition = position === DropPosition.Before ? newTargetIndex : newTargetIndex + 1
-
-  if (sourceIndex === newPosition) return true
 
   return false
 }
