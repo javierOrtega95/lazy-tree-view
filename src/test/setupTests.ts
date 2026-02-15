f5762a2db2adadbbac87d3204c1d8d9dc618b81b
javierOrtega95 <fjortega@wizzie.io>
2026-02-15 21:37:22 +0100

    fix: resolve jest-dom type errors in test files

--- src/test/setupTests.ts
+++ src/test/setupTests.ts
@@ -1,15 +1,6 @@
-declare module 'vitest' {
-  interface Assertion<T>
-    extends jest.Matchers<void, T>,
-      TestingLibraryMatchers<T, void> {}
-}
-
-import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers'
-import * as matchers from '@testing-library/jest-dom/matchers'
+import '@testing-library/jest-dom/vitest'
 import { cleanup } from '@testing-library/react'
-import { afterEach, expect } from 'vitest'
-
-expect.extend(matchers)
+import { afterEach } from 'vitest'
 
 afterEach(() => {
   cleanup()
